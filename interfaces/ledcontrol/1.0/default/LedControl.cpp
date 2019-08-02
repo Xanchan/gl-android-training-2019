161b7a71880c5dbbe232e36bb1a50a39ff365cf8
Oleksandr Siruk <alexsirook@gmail.com>
2019-08-02 17:15:21 +0300

    LedControlHal: Implement default LedControl
    Signed-off-by: Oleksandr Siruk <alexsirook@gmail.com>

--- interfaces/ledcontrol/1.0/default/LedControl.cpp
+++ interfaces/ledcontrol/1.0/default/LedControl.cpp
@@ -4,11 +4,20 @@
  */
 #define LOG_TAG "xan_LedControl_HAL"
 #include "LedControl.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
 
 namespace vendor {
 namespace globallogic {
 namespace ledcontrol {
 namespace V1_0 {
+
+using namespace std;
+
+vector <string> backupTrigger;
+vector <string> backupBrightness;
 
 LedControl::LedControl()
 {
@@ -18,6 +27,11 @@ LedControl::LedControl()
 Return<int32_t> LedControl::initializeLedControl(void)
 {
     ALOGI("initializeLedControl");
+    saveLedState();
+    for(int i = 1; i <= 4; i++){ 
+        setLedBrightness(i, LED_OFF);
+        setLedTrigger(i, LED_INIT_TRIGGER);
+    }
 
     return 0;
 }
@@ -25,15 +39,118 @@ Return<int32_t> LedControl::initializeLedControl(void)
 Return<int32_t> LedControl::terminateLedControl(void)
 {
     ALOGI("terminateLedControl");
+    if(backupTrigger.size() != 4 || backupBrightness.size() != 4) {
+        ALOGI("backup array size != 4 and trigger arr = %lu brightness arr = %lu", backupTrigger.size(), backupBrightness.size());
+    }
+    for(int i = 1; i <= 4; i++) {
+        setLedBrightness(i, backupBrightness[i - 1]);
+        setLedTrigger(i, backupTrigger[i - 1]);
+    }
 
     return 0;
 }
 
 Return<int32_t> LedControl::setLedState(Leds led, LedState state)
 {
+    string brightness;
+
     ALOGI("setLedState:%d %d", (int)led, (int)state);
- 
+    if((int)state){
+        brightness = LED_ON;
+    } else {
+        brightness = LED_OFF;
+    }
+    setLedBrightness((int)led, brightness);
+
     return 0;
+}
+
+Return<int32_t> LedControl::setLedTrigger(int led, string trigger) {
+    string path;
+    ofstream myFile;
+
+    ALOGI("setLedTrigger:%d %s", led, trigger.c_str());
+    path = getLedPath(led, LED_TRIGGER);
+    myFile.open(path);
+    if(myFile.is_open()){
+        myFile << trigger;
+        myFile.close();
+    }
+
+    return 0;
+}
+
+Return<int32_t> LedControl::setLedBrightness(int led, string brightness) {
+    string path;
+    ofstream myFile;
+
+    ALOGI("setLedBrightness:%d %s", led, brightness.c_str());
+    path = getLedPath(led, LED_BRIGHTNESS);
+    myFile.open(path);
+    if(myFile.is_open()){
+        myFile << brightness;
+        myFile.close();
+    }
+
+    return 0;
+}
+
+Return<string> LedControl::getLedPath(int num, string type) {
+    stringstream ss;
+
+    ss << LED_PATH << num << type;
+
+    return ss.str();
+}
+
+Return<string> LedControl::readLineFromFile(string path) {
+    ifstream myFile;
+    string strBuf;
+
+    if(path.length() != 0) {
+        myFile.open(path);
+        if (myFile.is_open()) {
+            getline(myFile, strBuf);
+            myFile.close();
+        } else {
+            ALOGI("init couldn't open file with path %s", path.c_str());
+        }
+    }
+
+    return strBuf;
+}
+
+Return<bool> LedControl::saveLedState(void) {
+
+    ALOGI("saveLedState");
+    for (int i = 1; i <= 4; i++){
+        string path, strBuff, triggerStr;
+        int startPos = 0, endPos = 0;
+
+        path = getLedPath(i, LED_TRIGGER);
+        strBuff = readLineFromFile(path);
+        startPos = strBuff.find("[") + 1;
+        endPos = strBuff.find("]");
+
+        if(startPos != 0 && endPos != 0) {
+            triggerStr = strBuff.substr(startPos, endPos - startPos);
+        } else {
+            ALOGI("cant find trigger value. startPos = %d, endPos = %d", startPos, endPos);
+            triggerStr = "none";
+        }
+
+        backupTrigger.push_back(triggerStr);
+
+        path = getLedPath(i, LED_BRIGHTNESS);
+        strBuff = readLineFromFile(path);
+
+        backupBrightness.push_back(strBuff);
+    }
+    for(int i = 0; i < 4; i++) {
+        ALOGI("led%d trigger = %s, brightness = %s", i + 1, backupTrigger[i].c_str(), backupBrightness[i].c_str());
+    }
+
+    return true;
 }
 
 }  // namespace V1_0
--- interfaces/ledcontrol/1.0/default/LedControl.h
+++ interfaces/ledcontrol/1.0/default/LedControl.h
@@ -9,6 +9,7 @@
 //#define LOG_TAG "LedControl_HAL"
 #include <vendor/globallogic/ledcontrol/1.0/ILedControl.h>
 #include <log/log.h>
+#include <string>
 
 namespace vendor {
 namespace globallogic {
@@ -16,6 +17,15 @@ namespace ledcontrol {
 namespace V1_0 {
 
 using namespace android::hardware;
+using namespace std;
+
+static const string LED_PATH = "/sys/class/leds/user_led";
+static const string LED_TRIGGER = "/trigger";
+static const string LED_INIT_TRIGGER = "none";
+static const string LED_BRIGHTNESS = "/brightness";
+static const string LED_ON = "255";
+static const string LED_OFF = "0";
+
 
 class LedControl: public ILedControl {
 public:
@@ -24,6 +34,12 @@ public:
     Return<int32_t> terminateLedControl(void) override;
     Return<int32_t> setLedState(Leds led, LedState state) override;
 
+private:
+    Return<bool> saveLedState(void);
+    Return<string> getLedPath(int num, string type);
+    Return<string> readLineFromFile(string path);
+    Return<int32_t> setLedTrigger (int led, string trigger);
+    Return<int32_t> setLedBrightness(int led, string brightness); 
 };
 }  // namespace V1_0
 }  // namespace ledcontrol
