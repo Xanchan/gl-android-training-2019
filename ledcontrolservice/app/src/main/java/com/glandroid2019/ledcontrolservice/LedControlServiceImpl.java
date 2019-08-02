4bcf958bdf42d8a9b1c2c15cd38c62db4f5f86aa
Oleksandr Siruk <alexsirook@gmail.com>
2019-08-02 16:08:32 +0300

    LedControl: Add HAL service connection and needed calls
    Signed-off-by: Oleksandr Siruk <alexsirook@gmail.com>

--- ledcontrolservice/app/src/main/java/com/glandroid2019/ledcontrolservice/LedControlServiceImpl.java
+++ ledcontrolservice/app/src/main/java/com/glandroid2019/ledcontrolservice/LedControlServiceImpl.java
@@ -3,22 +3,55 @@ import android.os.RemoteException;
 import android.util.Log;
 
 import com.glandroid2019.ledcontrolservice.IMyLedControlService;
+import vendor.globallogic.ledcontrol.V1_0.ILedControl;
+import vendor.globallogic.ledcontrol.V1_0.Leds;
+import vendor.globallogic.ledcontrol.V1_0.LedState;
 
 public class LedControlServiceImpl extends IMyLedControlService.Stub{
     private static final String LOG_TAG = "xan_ledcoserviceimpl";
+    private ILedControl ledService = null;
 
     public int initializeLedControl() {
-        Log.i(LOG_TAG, "init LedControl service");
+        try {
+            Log.i(LOG_TAG, "ILedControl get service");
+            ledService = ILedControl.getService(true);
+        } catch (RemoteException e) {
+            Log.e(LOG_TAG, "can't getService. remote exception");
+            return 1;
+        }
+
+        try {
+            if(ledService != null) {
+                ledService.initializeLedControl();
+            } else {
+                Log.e(LOG_TAG, "can't init LedControl. ledService == null");
+                return 1;
+            }
+        } catch (RemoteException e) {
+            Log.e(LOG_TAG, "can't init LedControl.");
+            return 1;
+        }
 
         return 0;
     }
 
     public void setLedState(int led, int state) {
-        Log.i(LOG_TAG, "setLedState: LED " + led + " state : " + state);
+        try {
+            Log.i(LOG_TAG, "LED " + led + " state : " + state);
+            ledService.setLedState((byte)led, (byte)state);
+        } catch (RemoteException e) {
+            Log.e(LOG_TAG, "can't setLedState");
+        }
     }
 
     public int terminateLedControl() {
-        Log.i(LOG_TAG, "terminateLedControl");
+        try {
+            Log.i(LOG_TAG, "terminateLedControl");
+            ledService.terminateLedControl();
+        } catch (RemoteException e) {
+            Log.e(LOG_TAG, "can't terminateLedControl");
+            return 1;
+        }
 
         return 0;
     }
